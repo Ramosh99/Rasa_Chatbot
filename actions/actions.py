e22eed859df946bd79727e22450480b1861a676a
Ramosh99 <einfetch@gmail.com>
2024-09-09 23:25:37 +0530

    Refactor action name and add booking information to CSV file

--- actions/actions.py
+++ actions/actions.py
@@ -3,30 +3,23 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import csv
+from datetime import datetime
 
-class ActionStoreData(Action):
-
+class ActionSaveBooking(Action):
     def name(self) -> Text:
-        return "action_store_data"
+        return "action_save_booking"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        # Get the current slot values
-        name = tracker.get_slot('name')
-
-        # Check if both name and email are provided
-        if name :
-            # Store both name and email in CSV
-            with open('user_data.csv', 'a', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([name])
-            
-            dispatcher.utter_message(text=f"Data stored: Name - {name}")
-
-        else:
-            # If neither name nor email is provided
-            dispatcher.utter_message(text="I need either your name or email to store your data.")
-
+        
+        room_type = tracker.get_slot('room_type')
+        date = tracker.get_slot('date')
+        time = tracker.get_slot('time')
+        
+        # Save the booking information to a CSV file
+        with open('bookings.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([room_type, date, time, datetime.now()])
+        
         return []
